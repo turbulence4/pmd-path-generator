bbe778624f0fb1d9883f2af3b73168d83bea4df6
turbulence4 <ultranoob533@gmail.com>
2026-06-17 15:37:07 -0400

    procedural terrain working!! my brain is fried

--- scripts/generateTerrain.gd
+++ scripts/generateTerrain.gd
@@ -1,51 +1,64 @@
+@tool
 extends MeshInstance3D
 
-@export var perlinScale = 3
-@export var numVertX = 10
-@export var numVertZ = 10
+var size := 256.0
+@export_range(4, 256, 4) var resolution := 32:
+	set(newResolution):
+		resolution = newResolution
+		updateMesh()
+
+@export var noise: FastNoiseLite:
+	set(newNoise):
+		noise = newNoise
+		if noise:
+			noise.changed.connect(updateMesh)
+
+@export_range(4.0, 128.0, 4.0) var height := 64.0:
+	set(newHeight):
+		height = newHeight
+		updateMesh()
 
 func _ready() -> void:
-	generateTerrain()
+	updateMesh()
 
-func generateTerrain():
-	var surfaceArray = []
-	surfaceArray.resize(Mesh.ARRAY_MAX)
+func getHeight(x: float, z: float) -> float:
+	return noise.get_noise_2d(x, z) * height
+
+func getNormal(x: float, z: float) -> Vector3:
+	var epsilon := size / resolution #the distance between adj verts in plane
+	var normal := Vector3(
+		(getHeight(x + epsilon, z) - getHeight(x - epsilon, z)) / (2.0 * epsilon), 
+		1.0,
+ 		(getHeight(x, z + epsilon) - getHeight(x, z - epsilon)) / (2.0 * epsilon),
+	)
 	
-	var verts = PackedVector3Array()
-	var uvs = PackedVector2Array()
-	var normals = PackedVector3Array()
-	var indices = PackedInt32Array()
+	return normal.normalized()
+
+func updateMesh():
+	var plane := PlaneMesh.new()
+	plane.subdivide_depth = resolution
+	plane.subdivide_width = resolution
+	plane.size = Vector2(size, size)
 	
-	var noise = FastNoiseLite.new()
+	var planeArrays := plane.get_mesh_arrays()
+	var vertexArray: PackedVector3Array = planeArrays[ArrayMesh.ARRAY_VERTEX]
+	var normalArray: PackedVector3Array = planeArrays[ArrayMesh.ARRAY_NORMAL]
+	var tangentArray: PackedFloat32Array = planeArrays[ArrayMesh.ARRAY_TANGENT]
 	
-	#prodgen code here
-	var noiseImage = noise.get_image(100, 100, false, true, true)
+	for i: int in vertexArray.size():
+		var vertex := vertexArray[i]
+		var normal := Vector3.UP
+		var tangent := Vector3.RIGHT
+		if noise:
+			vertex.y = getHeight(vertex.x, vertex.z)
+			normal = getNormal(vertex.x, vertex.z)
+			tangent = normal.cross(Vector3.UP)
+		vertexArray[i] = vertex
+		normalArray[i] = normal
+		tangentArray[4 * i] = tangent.x
+		tangentArray[4 * i + 1] = tangent.y
+		tangentArray[4 * i + 2] = tangent.z
 	
-	#make a square 300x300 units with 100x100 verts every 3 units
-	for z in range(numVertZ):
-		for x in range(numVertX):
-			verts.append(Vector3(x * perlinScale, 0, z * perlinScale))
-			normals.append(Vector3.UP)
-			uvs.append(Vector2(x, z))
-	
-	for z in range(numVertZ - 1):
-		for x in range(numVertX - 1):
-			var v0 = z * numVertX + x
-			var v1 = v0 + 1
-			var v2 = v0 + numVertX
-			var v3 = v2 + 1
-			
-			indices.append(v2)
-			indices.append(v0)
-			indices.append(v1)
-			
-			indices.append(v1)
-			indices.append(v3)
-			indices.append(v2)
-	
-	surfaceArray[Mesh.ARRAY_VERTEX] = verts
-	surfaceArray[Mesh.ARRAY_TEX_UV] = uvs
-	surfaceArray[Mesh.ARRAY_NORMAL] = normals
-	surfaceArray[Mesh.ARRAY_INDEX] = indices
-	
-	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surfaceArray)
+	var arrayMesh := ArrayMesh.new()
+	arrayMesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, planeArrays)
+	mesh = arrayMesh
