c91f23697c9527179f0d9eccfc5b24e1644444f6
turbulence4 <ultranoob533@gmail.com>
2026-06-17 12:54:04 -0400

    im going insane so im cutting myself off here

--- scripts/generateTerrain.gd
+++ scripts/generateTerrain.gd
@@ -1,6 +1,8 @@
-extends Node
+extends MeshInstance3D
 
-@onready var mesh = $"."
+@export var perlinScale = 3
+@export var numVertX = 10
+@export var numVertZ = 10
 
 func _ready() -> void:
 	generateTerrain()
@@ -14,9 +16,39 @@ func generateTerrain():
 	var normals = PackedVector3Array()
 	var indices = PackedInt32Array()
 	
+	var noise = FastNoiseLite.new()
+	
+	#prodgen code here
+	var noiseImage = noise.get_image(100, 100, false, true, true)
+	
+	#make a square 300x300 units with 100x100 verts every 3 units
+	for z in range(numVertZ):
+		for x in range(numVertX):
+			verts.append(Vector3(x * perlinScale, 0, z * perlinScale))
+			normals.append(Vector3.UP)
+			uvs.append(Vector2(x, z))
+	
+	for z in range(numVertZ - 1):
+		for x in range(numVertX):
+			var v0 = z * numVertX + x
+			var v1 = v0 + 1
+			var v2 = v0 + numVertX
+			var v3 = v2 + 1
+			
+			indices.append(v0)
+			indices.append(v2)
+			indices.append(v1)
+			
+			indices.append(v2)
+			indices.append(v3)
+			indices.append(v1)
+	
 	surfaceArray[Mesh.ARRAY_VERTEX] = verts
 	surfaceArray[Mesh.ARRAY_TEX_UV] = uvs
 	surfaceArray[Mesh.ARRAY_NORMAL] = normals
 	surfaceArray[Mesh.ARRAY_INDEX] = indices
 	
-	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surfaceArray)
+	print(verts)
+	print(indices)
+	
+	mesh.add_surface_from_arrays(Mesh.PRIMITIVE_LINES, surfaceArray)
