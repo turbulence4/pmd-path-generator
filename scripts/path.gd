7b592a2b348c933f9cdb7b862c279d81872311f9
turbulence4 <ultranoob533@gmail.com>
2026-06-19 23:58:21 -0400

    oh my fucking god path works YAY

--- scripts/path.gd
+++ scripts/path.gd
@@ -2,7 +2,7 @@ extends Path3D
 
 @onready var path = $"."
 var random := RandomNumberGenerator.new()
-var numPoints := random.randi_range(15, 50)
+var numPoints := random.randi_range(15, 150)
 
 @export var minSegLength := 3.0
 @export var maxSegLength := 20.0
@@ -16,19 +16,13 @@ func _ready() -> void:
 
 func getHeightFromTerrain(x: float, z: float) -> float:
 	var space_state = get_world_3d().direct_space_state
-	# use global coordinates, not local to node
+	#use global coordinates, not local to node
 	var query = PhysicsRayQueryParameters3D.create(Vector3(x, 1000, z), Vector3(x, -1000, z))
 	var result = space_state.intersect_ray(query)
 	
 	if result:
-		var sphere := MeshInstance3D.new()
-		sphere.mesh = SphereMesh.new()
-		sphere.scale = Vector3.ONE * 12
-		sphere.global_position = result.position
-		add_child(sphere)
 		return result.position.y
-	else:
-		return 0.0
+	return 0.0
 
 func generatePath():
 	var pathPointX := 0.0
@@ -43,13 +37,6 @@ func generatePath():
 		var worldPos = path.to_global(Vector3(pathPointX, 0, pathPointZ))
 		var height = getHeightFromTerrain(worldPos.x, worldPos.z)
 		path.curve.add_point(Vector3(worldPos.x, height, worldPos.z), Vector3.ZERO, Vector3.ZERO, i + 1)
-		
-		var posDisplay: Label3D = Label3D.new()
-		posDisplay.global_position = path.to_global(path.curve.get_point_position(i))
-		posDisplay.text = str(posDisplay.position)
-		posDisplay.billboard = BaseMaterial3D.BILLBOARD_ENABLED
-		posDisplay.font_size = 200
-		add_child(posDisplay)
 
 func _on_ui_regenerate_trail() -> void:
 	path.curve.clear_points()
